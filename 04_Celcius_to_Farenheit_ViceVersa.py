5162881ace45639d37d8b31bfa1e2111e7adbc10
Shreyas <shreyaslindota.3.435@gmail.com>
2026-08-09 22:54:18 +0530

    Commit fourth program

---
+++ 04_Celcius_to_Farenheit_ViceVersa.py
@@ -0,0 +1,12 @@
+print("Enter temperature:", end = " ")
+temp = int(input())
+print("Enter Scale of Temperature Celsius(C) or Farenheit(F):", end = " ")
+scale = input()
+if scale == 'C':
+    Farenheit = ((9 * temp) / 5) + 32
+    print("Temperature in Farenheit is:", Farenheit)
+elif scale == 'F':
+    Celsius = (5 * (temp - 32)) / 9
+    print("Temperature in Celcius is:", Celsius)
+else:
+    print("Incorrect Option, Please enter an appropriate scale!!!")
